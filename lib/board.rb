760048a2e50d0f9f57814515e1031c076d87ca2d
Chris Wegscheid <cwegscheid08@outlook.com>
2019-02-12 17:31:50 -0500

    Board display

--- lib/board.rb
+++ lib/board.rb
@@ -10,6 +10,7 @@ class Board
 	def place_piece(player, player_guess, x = 0)
 		if @board["column_#{player_guess}"][[player_guess-1,x]].nil?
 			@board["column_#{player_guess}"][[player_guess-1,x]] = player.game_piece
+			return [player_guess-1,x]
 		else
 			place_piece(player, player_guess, x+=1)
 		end
@@ -20,22 +21,43 @@ class Board
 	end
 
 	def full?
-		# @board.each_value { |value| puts "VALUE:#{value}"}
-		@board.each_value do |cells|
-			cells.each_value { |value| puts "VALUE:#{value}"}
+		@board.each_key do |key|
+			if @board[key].value?(nil)
+				return false
+			end
+			return true
 		end
 	end
 
 	def display
-		@board.each_key { |key| print "#{key.upcase} "}
+		@board.each_key { |key| print "#{key.upcase}  "}
 		puts
-		7.times { |x| puts "BOARD:#{@board['column_#{x+1}'].nil?}"}
-		# @board.each do 
-		# 	x = 0
-		# 	print "\n#{@board['column_#{x+1}']}\n\n"
-		# 	# print " VALUE:#{value}\n"
-		# 	@board['column_#{x+1}'].each { |y| puts "Y:#{y}"}
+
+		7.times do |x|
+			# x+=1
+			# print "BOARD:#{@board["column_#{x}"].pop} X:#{x}\n"
+			# @board["column_#{x}"]
+			# print "#{@board.values.reverse[x].invert.values} \n"
+			@board.each_value do |value|
+				# print @board[value][x].invert.values
+				# puts "VALUE:#{value} X:#{x}"
+				print "#{value.keys[6-x]}   "
+			end
+			puts
+		end
+
+		# @board.each_key do |key|
+
 		# end
+		
+	end
+
+	def fill
+		@board.each_key do |key|
+			@board[key].each_key do |cell|
+				@board[key][cell] = "CHRIS"
+			end
+		end
 	end
 
 
@@ -46,7 +68,7 @@ class Board
 		column = {}
 		x = 0
 		y = 0
-		6.times do
+		7.times do
 			7.times do
 				cell[[x,y]] = nil
 				y+=1
--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -9,77 +9,79 @@ RSpec.describe Game do
 		@game = Game.new("Chris")
 	end
 
-		describe "#who_is_playing" do
-			it "returns which player who's turn it is." do
-				@game.p1_turn = true
-				expect(@game.who_is_playing.class).to eql(Human)
-			end
-
-			it "return player_2 when p1_turn is false." do
-				@game.p1_turn = false
-				expect(@game.who_is_playing.class).to eql(Computer)
-			end
+	describe "#who_is_playing" do
+		it "returns which player who's turn it is." do
+			@game.p1_turn = true
+			expect(@game.who_is_playing.class).to eql(Human)
 		end
 
-		describe "#round" do
-
+		it "return player_2 when p1_turn is false." do
+			@game.p1_turn = false
+			expect(@game.who_is_playing.class).to eql(Computer)
 		end
+	end
 
-		# describe "#game_over?" do
-		# 	it "contiues play if no one has won." do
-		# 		# @game = Game.new("Chris")
-		# 		board.four_in_a_row? = true
-		# 		expect(@game.game_over?).to eql(true)
-		# 	end
 
-		# 	it "contiues to play the game if no one has won." do
-		# 		# @game = Game.new
-		# 		board.four_in_a_row? = false
-		# 		expect(@game.game_over?).to eql(round(who_is_playing))
-		# 	end
-		# end
 
-		# describe "#player_wins" do
-		# 	it "returns a string stating which player has won the game." do
-		# 		# @game = Game.new("Chris")
-		# 		board.four_in_a_row? = true
-		# 		expect(@game.game_over?).to eql("CHRIS has won!")
-		# 	end
+	# describe "#game_over?" do
+	# 	it "contiues play if no one has won." do
+	# 		# @game = Game.new("Chris")
+	# 		board.four_in_a_row? = true
+	# 		expect(@game.game_over?).to eql(true)
+	# 	end
 
-		# 	it "returns a string stating the player has lost if the computer wins." do
-		# 		# @game = Game.new("Chris")
-		# 		expect(@game.game_over?).to eql("You lose.")
-		# 	end
-		# end
+	# 	it "contiues to play the game if no one has won." do
+	# 		# @game = Game.new
+	# 		board.four_in_a_row? = false
+	# 		expect(@game.game_over?).to eql(round(who_is_playing))
+	# 	end
+	# end
+
+	# describe "#player_wins" do
+	# 	it "returns a string stating which player has won the game." do
+	# 		# @game = Game.new("Chris")
+	# 		board.four_in_a_row? = true
+	# 		expect(@game.game_over?).to eql("CHRIS has won!")
+	# 	end
+
+	# 	it "returns a string stating the player has lost if the computer wins." do
+	# 		# @game = Game.new("Chris")
+	# 		expect(@game.game_over?).to eql("You lose.")
+	# 	end
+	# end
 end
 
-# RSpec.describe Board do
-# 	describe "#place_piece" do
-# 		it "returns the cell that the current player has put their piece in." do
-# 			board = Board.new
-# 			expect(board.place_piece(player_1, 4)).to eql([3,0])
-# 		end
+RSpec.describe Board do
+	before do
+		@board = Board.new
+	end
 
-# 		it "returns the next cell equal to nil in the same column as the user input." do
-# 			board = Board.new
-# 			board.cell[5,0] = player_1
-# 			board.cell[5,1] = player_2
-# 			expect(board.place_piece(player_1, 6)).to eql([5,2])
-# 		end
-# 	end
+	describe "#full" do
+		context "it returns true or false based on cell availability." do
+			it "returns false if any board value is equal to NIL." do 
+				expect(@board.full?).to eql(false)
+			end
 
-# 	describe "#full?" do
-# 		it "returns false if there are still cells that are equal to nil." do
-# 			board = Board.new
-# 			expect(board.full?).to eql(false)
-# 		end
+			it "returns true if no board value is equal to NIL." do 
+				@board.fill
+				expect(@board.full?).to eql(true)
+			end
+		end
+	end
 
-# 		it "returns true if every cell on the board has a game_piece in them." do
-# 			board = Board.new
-# 			expect(board.full?).to eql(true)
-# 		end
-# 	end
-# end
+	describe "#place_piece" do
+		it "returns the cell that the current player has put their piece in." do
+			player_1 = Human.new("Chris", "C")
+			expect(@board.place_piece(player_1, 4)).to eql([3,0])
+		end
+
+		it "returns the next cell equal to nil in the same column as the user input." do
+			player_1 = Human.new("Chris", "C")
+			@board.place_piece(player_1, 6)
+			expect(@board.place_piece(player_1, 6)).to eql([5,1])
+		end
+	end
+end
 
 
 # RSpec.describe Human do
